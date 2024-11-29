b397bebb4ae51a49e3dca56568a0b21221bb2ebc
vladislav.verbitsky <vladislav.verbitsky@nasoft888.dev>
2024-11-29 14:29:19 +0300

    init: add game starter && setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
-package cmd
+package main
+
+import (
+	"log"
+	"tower-defense/internal/setup"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+
+	if err := ebiten.RunGame(&setup.Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ internal/setup/dev.go
@@ -0,0 +1,20 @@
+package setup
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
